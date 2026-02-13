214c3cc1fe222b57ab62e3f28e720e24a7d3af7e
Levelvini <viniciusfc29@gmail.com>
2026-02-13 11:47:50 -0300

    feat: application.properties and Transaction have been created;

---
+++ src/main/java/com/levelvini/picpay_simplificado/model/enums/Transaction.java
@@ -0,0 +1,27 @@
+package com.levelvini.picpay_simplificado.model.enums;
+
+import com.levelvini.picpay_simplificado.model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender-id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver-id")
+    private User receiver;
+    private LocalDateTime timestampp;
+}
