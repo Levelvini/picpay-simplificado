cab7d9ebd47df382cf06646d2131eaf2f623f1c1
Levelvini <viniciusfc29@gmail.com>
2026-02-13 10:41:38 -0300

    first commit

---
+++ src/test/java/com/levelvini/picpay_simplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelvini.picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
