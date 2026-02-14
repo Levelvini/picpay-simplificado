2127d858b3ebca08a26a1611edec2c663f78b4b5
Levelvini <viniciusfc29@gmail.com>
2026-02-14 09:33:32 -0300

    refactor: atribute names has changed

--- src/main/java/com/levelvini/picpay_simplificado/model/User.java
+++ src/main/java/com/levelvini/picpay_simplificado/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private String firstName;
     private String lastName;
     @Column(unique = true)
-    private String CPF;
+    private String document;
     @Column(unique = true)
     private String email;
     private String password;
