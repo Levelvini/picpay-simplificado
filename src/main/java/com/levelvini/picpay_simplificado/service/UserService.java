7c376ee712968dc31ebc944ce8bcdcdeefd895d8
Levelvini <viniciusfc29@gmail.com>
2026-02-14 09:32:12 -0300

    feat: create UseService

---
+++ src/main/java/com/levelvini/picpay_simplificado/service/UserService.java
@@ -0,0 +1,41 @@
+package com.levelvini.picpay_simplificado.service;
+
+import com.levelvini.picpay_simplificado.exceptions.InvalidUserTypeException;
+import com.levelvini.picpay_simplificado.exceptions.ResourceNotFoundException;
+import com.levelvini.picpay_simplificado.model.User;
+import com.levelvini.picpay_simplificado.model.enums.UserType;
+import com.levelvini.picpay_simplificado.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validateTransaction(User sender, BigDecimal amount){
+        if (sender.getUserType() == UserType.MERCHANTS){
+            throw new InvalidUserTypeException("Usuario do tipo logista não está autorizado a realizar transações");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new ArithmeticException("saldo insuficiente");
+        }
+    }
+    @Transactional
+    public Optional<User> findUserById(){
+        return Optional.of(findUserById().orElseThrow(()-> new ResourceNotFoundException("user not founded")));
+    }
+
+    @Transactional
+    public void saveUser(User user){
+        repository.save(user);
+    }
+}
