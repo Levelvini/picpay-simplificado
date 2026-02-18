75e411520e74ed0b361708ea8094d2334b0d3592
Levelvini <viniciusfc29@gmail.com>
2026-02-18 17:25:22 -0300

    feat: AuthorizationException created

---
+++ src/main/java/com/levelvini/picpay_simplificado/exceptions/AuthorizationException.java
@@ -0,0 +1,7 @@
+package com.levelvini.picpay_simplificado.exceptions;
+
+public class AuthorizationException extends Exception{
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
